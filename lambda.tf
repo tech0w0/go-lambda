3f889f95cbf1f1319d6821f56bbb64ada6a81c53
Catalina <tech0w0@gmail.com>
2020-05-13 20:17:19 +0200

    Removed unnecessary files

--- lambda.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_lambda_function" "function" {
-  filename      = "deployment.zip"
-  function_name = "Simple"
-  role          = "${aws_iam_role.lambda_role.arn}"
-  handler       = "main"
-  runtime       = "go1.x"
-}
--- s3.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_s3_bucket" "bucket" {
-  bucket = "go-lambda2"
-  acl    = "private"
-}
